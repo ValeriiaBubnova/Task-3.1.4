0f31610fed1ec816a5b65a595535895391bd84d4
ValeriiaBubnova <paulo.insains@gmail.com>
2025-02-09 00:30:25 +0300

    Add files via upload

---
+++ src/main/java/com/bubnova/ITM/Task_314/Application.java
@@ -0,0 +1,16 @@
+package com.bubnova.ITM.Task_314;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		RestTemplate restTemplate = new RestTemplate();
+
+
+	}
+
+}
